1ce68d4421b84d076c0ac34417446939726b08c9
sohaibahmed368 <sohaibahmed368@gmail.com>
2023-02-08 22:30:39 +0500

    First commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+let pickupPoint = document.querySelector('#pickup_point');
+let dropPoint = document.querySelector('#drop_point');
+
+let pickupAutoComplete = new google.maps.places.Autocomplete(pickupPoint, {
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+});
+pickupAutoComplete.addListener("place_changed", function(){
+    let place = autocomplete.getPlace();
+});
+
+let dropPointAutoComplete = new google.maps.places.Autocomplete(dropPoint, {
+    fields: ["address_components", "geometry"],
+    types: ["address"],
+});
+dropPointAutoComplete.addListener("place_changed", function(){
+    let place = autocomplete.getPlace();
+});
